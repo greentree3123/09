7d0d624a71a0ab163e4c8154b6f0517e42c900c8
Kim hajeong <happykhj67@gmail.com>
2023-11-28 00:45:15 +0900

    [add] posts/views.py

--- database09/posts/views.py
+++ database09/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
 # Create your views here.
+def post_create_view(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'main.html')
+    else:
+        form = PostForm()
+
+    return render(request, 'writeup.html', {'form': form})
