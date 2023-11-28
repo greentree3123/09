df6afdf28de48cc6ed163e4316bcf6230573f8d9
Kim hajeong <happykhj67@gmail.com>
2023-11-28 17:54:50 +0900

    [add] main으로 돌아오게 하기

--- database09/posts/urls.py
+++ database09/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import post_create_view
+from .views import post_create_view, main_view
 
 app_name='posts'
 urlpatterns = [
     path('new/',post_create_view,name='post-create'),
+    path('main/',main_view,name='main'),
 ]
--- database09/posts/views.py
+++ database09/posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Post
 from .forms import PostForm
 
 # Create your views here.
@@ -7,8 +8,13 @@ def post_create_view(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return render(request, 'main.html')
+            new_post = form.save()
+            return redirect('main', post_id=new_post.id)  # post_id를 메인 뷰에 전달
     else:
         form = PostForm()
 
     return render(request, 'writeup.html', {'form': form})
+
+def main_view(request, post_id=None):
+    posts = Post.objects.all()
+    return render(request, 'main.html', {'posts': posts, 'new_post_id': post_id})
