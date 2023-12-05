7edfaf43a2c1e32aefb30a55c2bd43e8c9ead8db
Kim hajeong <happykhj67@gmail.com>
2023-12-06 01:15:50 +0900

    [fix] 한국시간으로 띄우기 위해 코드 수정

--- database09/config/settings.py
+++ database09/config/settings.py
@@ -132,3 +132,5 @@ LOGIN_FIELDS=['email']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TIME_ZONE = 'Asia/Seoul'
