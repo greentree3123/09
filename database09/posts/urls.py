2714f9c6249734673df4b981626e038f3e924f19
moonsuho <msh1593@naver.com>
2023-12-08 00:55:38 +0900

    .

--- database09/posts/urls.py
+++ database09/posts/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
-from .views import SearchFormView, post_create_view, main_view, post_detail_view, post_list_view, post_update_view, comment_delete_view, myPost_view, likes
+from .views import SearchFormView, post_create_view, main_view, post_detail_view, post_list_view, post_update_view, comment_delete_view, likes
 
 
 app_name='posts'
 urlpatterns = [
     path('new/',post_create_view,name='post-create-view'),
     path('main/',main_view,name='main'),
-    path('<int:id>/', post_detail_view, name='post-detail'),
-    path('<int:id>/edit/', post_update_view, name='post-update'),
+    path('<int:article_pk>/', post_detail_view, name='post-detail'),
+    path('<int:article_pk>/edit/', post_update_view, name='post-update'),
     path('main/', SearchFormView.as_view(), name='search'),
     path('main/',post_list_view,name='main'),
-    path('myPost/', myPost_view, name='myPost'),
     path('comment/<int:comment_id>/delete/', comment_delete_view, name='comment-delete'),
     path('<int:article_pk>/likes/', likes, name='likes'),
 ]
